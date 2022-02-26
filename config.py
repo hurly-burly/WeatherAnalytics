92231dee86326866d64672d8a47588b15b37a96c
hurly-burly <58910643+hurly-burly@users.noreply.github.com>
2022-02-26 19:07:16 +0530

    new files

--- config.py
+++ config.py
@@ -7,3 +7,7 @@ DATASET1 = 'DATASET1'
 DATASET2 = 'DATASET2'
 
 BASE_DIR = '/var/lib'  # path in container instance
+
+# local paths for logs and DB replication
+REPLICATED_LOG_DIR_PATH = 'logs'
+REPLICATED_DB_DIR_PATH = 'SQLiteDB'
